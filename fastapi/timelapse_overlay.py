5fa01cff09120b79ec27e469b33efd94716fdf78
David Samtani <109256466+Bevid11@users.noreply.github.com>
2025-08-01 19:41:02 -1000

    Update timelapse_overlay.py

--- fastapi/timelapse_overlay.py
+++ fastapi/timelapse_overlay.py
@@ -2,11 +2,10 @@ import os
 import fastapi_astrometry
 from fastapi_timelapse import stitch_timelapse
 
-def timelapse_with_overlay(constellations: str, stars: str, planets: str, date_of_timelapse: str):
+def timelapse_with_overlay(movie_out_name, constellations: str, stars: str, planets: str, date_of_timelapse: str):
     image_in_dir = f'/data/cloudcams/cloudcam2025/{date_of_timelapse}_movie'
     image_out_dir = '/home/akami-3/gitlabsource/CloudCams/requested_overlay_images'
-    movie_out_name = f'requested_overlay_movie'
-    movie_out_dir = '/data/cloudcams/cloudcam2025/images'
+    movie_out_dir = '/data/cloudcams/cloudcam2025'
 
     for img_name in os.listdir(image_in_dir):
         img_path = os.path.join(image_in_dir, img_name)
